56914fb605bc139ed5c2be4c5cb6065051072c88
Emile <emile.bispo@etec.sp.gov.br>
2025-06-03 20:31:02 -0300

    classefuncionario

---
+++ ClasseFuncionario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Funcionario
+
+    {
+      
+        {
+           public string Nome {  get; set; }
+           public string Cpf { get; set; }
+           public string Cargo { get; set; }
+           public double Salario { get; set; }
+        
+        public Funcionario(string nome, string cpf, string cargo, double salario)
+        {
+            Nome = nome;
+            Cpf = cpf;
+            Cargo = cargo;
+            Salario = salario;
+        }
+
+            public void ExibirDados()
+        {
+            Console.WriteLine($"Nome: {Funcionario.Nome}");
+            Console.WriteLine($"Cpf: {Funcionario.Cpf}");
+            Console.WriteLine($"Cargo: {Funcionario.Cargo}");
+            Console.WriteLine($"Cargo: {Funcionario.Salario}"};
+        }
+    }
+}
+    
+
