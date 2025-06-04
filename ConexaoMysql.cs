39ab86b6d55300e993d3004f5ea38e2be816c3c4
Alexsander <alexsander.santana2@etec.sp.gov.br>
2025-06-03 22:15:29 -0300

    mais testes de conexao criacao e listagem

--- ConexaoMysql.cs
+++ ConexaoMysql.cs
@@ -15,14 +15,21 @@ namespace GestaoMercado
             get { return _login.ToUpper().Trim(); }
             set { _login = value; }
         }
+        private string _senha;
+        public string Senha
+        {
+            get { return _senha; }
+            set { _senha = value; }
+        }
     }
     internal class ConexaoMysql
     {
         private static string servidor = "localhost";
+        private static string porta = "3307";
         private static string banco_de_dados = "sistema_mercado";
         private static string usuario = "root";
         private static string senha = "root";
-        private static string dsn = "server=" + servidor + ";database=" + banco_de_dados + ";user=" + usuario + ";password=" + senha;
+        private static string dsn = "server=" + servidor + ";port=" + porta + ";database=" + banco_de_dados + ";user=" + usuario + ";password=" + senha;
         private MySqlConnection conexao = new MySqlConnection(dsn);
         public InformacoesUsuario Login(string login, string senha)
         {
@@ -62,6 +69,34 @@ namespace GestaoMercado
             int afetadas = cmd.ExecuteNonQuery();
             conexao.Close();
             return afetadas;
+        }
+        public InformacoesUsuario[] ListarUsuarios()
+        {
+            conexao.Open();
+            string sql = "SELECT *, COUNT(id_usuario) AS numero_de_usuarios FROM usuarios";
+            MySqlCommand cmd = new MySqlCommand(sql, conexao);
+            MySqlDataReader busca = cmd.ExecuteReader();
+            InformacoesUsuario[] retorno = [];
+            uint numero_de_resultados = 0;
+            if (busca.HasRows)
+            {
+                uint i = 0;
+                while (busca.Read())
+                {
+                    if (numero_de_resultados == 0)
+                    {
+                        numero_de_resultados = (uint) busca.GetInt32("numero_de_usuarios");
+                        retorno = new InformacoesUsuario[numero_de_resultados];
+                    }
+                    retorno[i] = new InformacoesUsuario();
+                    retorno[i].Nome = busca.GetString("nome_usuario");
+                    retorno[i].Login = busca.GetString("login_usuario");
+                    retorno[i].Senha = busca.GetString("senha_usuario");
+                    i++;
+                }
+            }
+            conexao.Close();
+            return retorno;
         }
     }
 }
