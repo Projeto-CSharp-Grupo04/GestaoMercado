9c061058a7cd6f80e894b051e6ee333bb7c28429
Alexsander <alexsander.santana2@etec.sp.gov.br>
2025-06-03 22:17:30 -0300

    inicio telas usando forms

--- Program.cs
+++ Program.cs
@@ -1,43 +1,17 @@
-﻿namespace GestaoMercado
+namespace Forms
 {
-    internal class Program
+    internal static class Program
     {
-        public static void CriarNovoUsuario()
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            ConexaoMysql conexao = new ConexaoMysql();
-            Console.WriteLine("digite o novo login");
-            string login = Console.ReadLine();
-            Console.WriteLine("digite a nova senha");
-            string senha = Console.ReadLine();
-            conexao.CriarUsuario(login, senha);
-        }
-        public static void LogarNoUsuario()
-        {
-            ConexaoMysql conexao = new ConexaoMysql();
-            Console.WriteLine("digite o seu login");
-            string login = Console.ReadLine();
-            Console.WriteLine("digite a sua senha");
-            string senha = Console.ReadLine();
-            conexao.Login(login, senha);
-        }
-        public static void Main()
-        {
-            try
-            {
-                Menu menu = new Menu();
-                menu.MostrarApresentacao();
-                menu.MostrarComandos();
-            }
-            catch (Exception excecao)
-            {
-                Console.WriteLine("Erro: {0}", excecao.Message);
-                Console.WriteLine("Stack trace: {0}", excecao.StackTrace);
-            }
-            finally
-            {
-                Console.WriteLine("Pressione qualquer tecla para sair ...");
-                Console.ReadKey();
-            }
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Tela_de_Login());
         }
     }
 }
---
+++ Tela_de_Login.cs
@@ -0,0 +1,10 @@
+namespace Forms
+{
+    public partial class Tela_de_Login : Form
+    {
+        public Tela_de_Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
