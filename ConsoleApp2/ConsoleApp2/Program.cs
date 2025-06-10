bd8a34f2f8e9666fed5ecb12ff413f0302eb0ba6
Emile Stefane Cerqueira Bispo <emile.bispo@etec.sp.gov.br>
2025-06-09 21:56:13 -0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Estoque
+    {
+        public string NomeProduto;
+        public int Quantidade;
+        public void Adicionar(int quantidade)
+        {
+            Quantidade = quantidade;
+        }
+
+        public void Remover(int quantidade)
+        {
+            if (quantidade <= Quantidade)
+            {
+                Quantidade -= quantidade;
+            }
+            else
+            {
+                Console.WriteLine("Quantidade insuficiente no estoque.");
+            }
+        }
+        public void MostrarEstoque()
+        {
+            Console.WriteLine($"Produto:{NomeProduto}, Qunatidade: {Quantidade}");
+        }
+    }
+}
